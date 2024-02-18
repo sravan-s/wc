3c893567712d3b21ec30aecf30c2ac24e1abccb2
sravan <sravan-s@protonmail.com>
2024-02-19 00:31:52 +0900

    parse the arguments

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,55 @@
+let version = "0.0.0"
+let help = "
+  Usage: wc --help | --version | [OPTION]... FILE
+  Options:
+    -c                    print the byte counts
+    -l                    print the newline counts
+    -w                    print the word counts
+    -m                    print the character counts
+    --help                display this help and exit
+    --version             output version information and exit
+"
+
+type file_operation = {
+  mutable byte_count: bool;
+  mutable newline_count: bool;
+  mutable word_count: bool;
+  mutable character_count: bool;
+  mutable file: string;
+} [@@deriving show]
+
+type operation =
+  | Help
+  | Version
+  | File_Operation of file_operation
+
+let rec parse_args args =
+  let op = { byte_count = false; newline_count = false; word_count = false; character_count = false; file = "" } in
+  match args with
+    | [] -> Error "No file specified"
+    | ["--help"] -> Ok(Help)
+    | ["--version"] -> Ok(Version)
+    | "-c" :: t' -> op.byte_count <- true; parse_args t'
+    | "-l" :: t' -> op.newline_count <- true; parse_args t'
+    | "-w" :: t' -> op.word_count <- true; parse_args t'
+    | "-m" :: t' -> op.character_count <- true; parse_args t'
+    | [f] -> op.file <- f; Ok(File_Operation op)
+    | _ -> Error "Invalid arguments"
+
+let _ = Sys.argv
+  |> Array.to_list
+  |> List.tl (* first argument seems to be path to executable, so remove it *)
+  |> parse_args
+  |> function
+    | Ok(Help) -> print_endline help
+    | Ok(Version) -> print_endline version
+    | Ok(File_Operation op) -> print_endline (show_file_operation op)
+    | Error e -> print_endline e
+
+
+(* Util to print arguments *)
+(* let print_argv () =
+    Array.iteri (fun i arg -> Printf.printf "Argument %d: %s\n" (i + 1) arg) Sys.argv
 let () =
-  for i = 0 to Array.length Sys.argv - 1 do
-    Printf.printf "[%i] %s\n" i Sys.argv.(i)
-  done
+    print_argv () *)
 
